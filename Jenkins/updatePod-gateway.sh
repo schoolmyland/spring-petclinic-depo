d5fc4e9e7dfbbf3fd46318c18e301bc0a2f9fa59
schoolmyland <145770656+schoolmyland@users.noreply.github.com>
2024-08-20 15:48:35 +0200

    Faux Positif

--- Jenkins/updatePod-gateway.sh
+++ Jenkins/updatePod-gateway.sh
@@ -5,3 +5,11 @@ if [ $? -ne 0 ]; then
   echo "Contact to the cluster have failed "
   exit 2
 fi
+
+podName=$(kubectl get pods -n $2 -o jsonpath='{.items[*].metadata.name}' | tr ' ' '\n' | grep '^api-gateway')
+if [ -z "$podName" ] || [[ "$podName" == *"resource(s) were provided, but no name was specified"* ]]; then
+  echo "This customers doesn't have any namespace on our cloud"
+  exit 0
+fi
+
+kubectl delete pod $podName -n $2
